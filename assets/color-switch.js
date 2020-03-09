bf9141ef3d7ad60674a51c5196d4e6370dabf212
nautatva <nnautatva@gmail.com>
2020-03-09 15:41:42 +0530

    Manual switch to light/dark modes

---
+++ assets/color-switch.js
@@ -0,0 +1,66 @@
+document.documentElement.classList.remove('no-js');
+
+const STORAGE_KEY = 'user-color-scheme';
+const COLOR_MODE_KEY = '--color-mode';
+
+const modeToggleButton = document.querySelector('.js-mode-toggle');
+const modeToggleText = document.querySelector('.js-mode-toggle-text');
+const modeStatusElement = document.querySelector('.js-mode-status');
+
+const getCSSCustomProp = propKey => {
+  let response = getComputedStyle(document.documentElement).getPropertyValue(propKey);
+
+  if (response.length) {
+    response = response.replace(/\"/g, '').trim();
+  }
+
+  return response;
+};
+
+// The following function is not working as expected
+const setButtonLabelAndStatus = currentSetting => {
+  console.log(currentSetting)
+  console.log(currentSetting === 'dark');
+  modeToggleText.innerText = `Enable ${currentSetting === 'dark' ? 'light' : 'dark'} mode`;
+  modeStatusElement.innerText = `Color mode is now ${currentSetting}`;
+};
+
+const applySetting = passedSetting => {
+  let currentSetting = passedSetting || localStorage.getItem(STORAGE_KEY);
+
+  if (currentSetting) {
+    document.documentElement.setAttribute('data-user-color-scheme', currentSetting);
+    setButtonLabelAndStatus(currentSetting);
+  } else {
+    setButtonLabelAndStatus(getCSSCustomProp(COLOR_MODE_KEY));
+  }
+};
+
+const toggleSetting = () => {
+  let currentSetting = localStorage.getItem(STORAGE_KEY);
+
+  switch (currentSetting) {
+    case null:
+      currentSetting = getCSSCustomProp(COLOR_MODE_KEY) === 'dark' ? 'light' : 'dark';
+      break;
+    case 'light':
+      currentSetting = 'dark';
+      break;
+    case 'dark':
+      currentSetting = 'light';
+      break;
+  }
+
+  localStorage.setItem(STORAGE_KEY, currentSetting);
+
+  return currentSetting;
+};
+
+
+modeToggleButton.addEventListener('click', evt => {
+  evt.preventDefault();
+
+  applySetting(toggleSetting());
+});
+
+applySetting();
